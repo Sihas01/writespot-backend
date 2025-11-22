b483748bfd41021890b04ed5793bdec38617d933
nadeesh71 <nadeeshfernando327@gmail.com>
2025-11-22 23:14:04 +0530

    auth backend

---
+++ src/utils/config.js
@@ -0,0 +1,9 @@
+require('dotenv').config(); // load .env first
+
+module.exports = {
+  MONGO_URI: process.env.MONGO_URI,
+  JWT_SECRET: process.env.JWT_SECRET,
+  PORT: process.env.PORT || 5000,
+  EMAIL: process.env.EMAIL,
+  EMAIL_PASS: process.env.EMAIL_PASS,
+};
